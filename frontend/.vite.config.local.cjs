6403e2497ab35faf5b6bf7213844000cb062f6f2
Albert Anguera Sempere <albert.anguera.sempere@gmail.com>
2025-08-12 15:35:17 +0200

    added allowed domain

--- frontend/.vite.config.local.cjs
+++ frontend/.vite.config.local.cjs
@@ -1,6 +1,7 @@
 // Useful when running backend locally on 8000 without Docker
 module.exports = {
   server: {
+    allowedHosts: ['genai-code-generator.duckdns.org'],
     proxy: {
       '/api': {
         target: 'http://localhost:8000',
